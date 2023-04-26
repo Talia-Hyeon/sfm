c202c739d4dc75bb639bafd19ffd4eee66c1fade
Talia-Hyeon <030310jhkim@naver.com>
2023-04-26 10:17:47 +0900

    view_list로 수정

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 def read_files(path):
     txt_file = osp.join(path, 'K.txt')
     K = np.loadtxt(txt_file)
-    img_file = [osp.join(path, file) for file in os.listdir(path) if file.endswith('.JPG')]
-    return K, img_file
+    img_filess = [osp.join(path, file) for file in os.listdir(path) if file.endswith('.JPG')]
+    return K, img_filess
 
 
 def extract_feature(img):
@@ -19,13 +19,24 @@ def extract_feature(img):
     # feature = cv2.ORB_create()
     kp, desc = feature.detectAndCompute(img, None)
 
+    return kp, desc
+
+
+def create_view(self, img_files):
+    view_l = []
+    for i in img_files:
+        i_img = cv2.imread(i, cv2.COLOR_BGR2RGB)
+        kp, desc = extract_feature(i_img)
+        view = {['img_path']: i, ['kp']: kp, ['desc']: desc}
+        view_l.append(view)
+
     # draw keypoints
+    img = cv2.imread(view_l[0]['img_path'], cv2.COLOR_BGR2RGB)
+    kp = view_l[0]['kp']
     img_draw = cv2.drawKeypoints(img, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     plt.imshow(img_draw)
-    plt.show(block=False)
-    plt.pause(2)
-    plt.close()
-    return kp, desc
+    plt.show()
+    return view_l
 
 
 def match_keypoints(img1, kp1, desc1, img2, kp2, desc2):
@@ -60,13 +71,6 @@ def essentialMat_estimation(kp1, kp2, good_matches, K):
     return R, t, pts1, pts2
 
 
-def init_projMat(K, R, t):
-    Rt0 = np.hstack((np.eye(3), np.zeros((3, 1))))  # 1st camera coordinate: world coordinate
-    Rt1 = np.hstack((R, t))
-    Rt1 = np.matmul(K, Rt1)
-    return Rt0, Rt1
-
-
 def triangulate(R, t, K, p1, p2):
     Rt0 = np.hstack((np.eye(3), np.zeros((3, 1))))  # 1st camera coordinate: world coordinate
     Rt1 = np.hstack((R, t))
@@ -77,49 +81,8 @@ def triangulate(R, t, K, p1, p2):
 
     p3d = cv2.triangulatePoints(Rt0, Rt1, pt1, pt2)
     p3d /= p3d[3]  # Homogeneous Coordinate
-    # p3d's shape: (4,1,80)  4:x,y,z +?
+    # p3d's shape: (4,1,80)  4:x,y,z,1
     return p3d
-
-
-# Rescale to Homogeneous Coordinate
-def rescale_point(pts1, pts2, length):
-    p1 = [[]]
-    p2 = [[]]
-    for i in range(length):
-        tmp1 = pts1[i].flatten()
-        tmp1 = np.append(tmp1, 1)
-        p1 = np.append(p1, tmp1)
-        tmp2 = pts2[i].flatten()
-        tmp2 = np.append(tmp2, 1)
-        p2 = np.append(p2, tmp2)
-
-    p1 = p1.reshape((length), 3)
-    p2 = p2.reshape((length), 3)
-    return p1, p2
-
-
-# Triangulation
-def LinearTriangulation(Rt0, Rt1, p1, p2):
-    A = [p1[1] * Rt0[2, :] - Rt0[1, :],  # x(p 3row) - (p 1row)
-         -(p1[0] * Rt0[2, :] - Rt0[0, :]),  # y(p 3row) - (p 2row)
-         p2[1] * Rt1[2, :] - Rt1[1, :],  # x'(p' 3row) - (p' 1row)
-         -(p2[0] * Rt1[2, :] - Rt1[0, :])]  # y'(p' 3row) - (p' 2row)
-
-    A = np.array(A).reshape((4, 4))
-    AA = A.T @ A
-    U, S, VT = np.linalg.svd(AA)  # right singular vector
-
-    return VT[3, 0:3] / VT[3, 3]
-
-
-def make_3dpoint(Rt0, Rt1, p1, p2):
-    p3ds = []
-    for pt1, pt2 in zip(p1, p2):
-        p3d = LinearTriangulation(Rt0, Rt1, pt1, pt2)
-        # p3d.shape: (3,)
-        p3ds.append(p3d)
-    p3ds = np.array(p3ds).T
-    return p3ds
 
 
 def reconstruct(K, img1, img2):
@@ -127,49 +90,29 @@ def reconstruct(K, img1, img2):
     kp2, desc2 = extract_feature(img2)
     good_matches = match_keypoints(img1, kp1, desc1, img2, kp2, desc2)
     R, t, pts1, pts2 = essentialMat_estimation(kp1, kp2, good_matches, K)
-    # point_3d = triangulate(R, t, K, pts1, pts2)
-    Rt0, Rt1 = init_projMat(K, R, t)
-    p1, p2 = rescale_point(pts1, pts2, len(pts1))
-    point_3d = make_3dpoint(Rt0, Rt1, p1, p2)
+    point_3d = triangulate(R, t, K, pts1, pts2)
     return point_3d
 
 
-def visualization(p3ds):
-    X = np.array([])
-    Y = np.array([])
-    Z = np.array([])  # 120
-    X = np.concatenate((X, p3ds[0]))
-    Y = np.concatenate((Y, p3ds[1]))
-    Z = np.concatenate((Z, p3ds[2]))
-
-    fig = plt.figure(figsize=(15, 15))
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(X, Y, Z, c='b', marker='o')
-    plt.show()
-
-
 def run(path):
-    K, img_file = read_files(img_root)
+    K, img_files = read_files(img_root)
 
     # baseline
-    img1 = img_file[0]
-    img2 = img_file[1]
+    img1 = img_files[0]
+    img2 = img_files[1]
     img1 = cv2.imread(img1, cv2.COLOR_BGR2RGB)
     img2 = cv2.imread(img2, cv2.COLOR_BGR2RGB)
     point_3d = reconstruct(K, img1, img2)
 
     # not baseline
-    for i in range(2, len(img_file)):
-        img2 = img_file[i]
+    for i in range(2, len(img_files)):
+        img2 = img_files[i]
         img2 = cv2.imread(img2, cv2.COLOR_BGR2RGB)
         point_3d_tem = reconstruct(K, img1, img2)
         point_3d = np.hstack((point_3d, point_3d_tem))
         print("point_3d's shape: {}".format(point_3d.shape))
 
-    visualization(point_3d)
-
 
 if __name__ == '__main__':
     img_root = '../data/'
     run(img_root)
-    # test
